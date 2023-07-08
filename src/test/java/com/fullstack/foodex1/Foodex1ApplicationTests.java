df3ffcff7e3cb581d9006e4297ee9c28b5d471ed
Akash Bachhav <107236708+theskyb11@users.noreply.github.com>
2023-07-08 15:45:20 +0530

    Added files

---
+++ src/test/java/com/fullstack/foodex1/Foodex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.foodex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Foodex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
