ada303e0bd1aa5002fd41e8e665055b302529b6a
Akash Bachhav <akashbachhav2003@gmail.com>
2023-07-15 22:48:58 +0530

    navbar updated

--- src/main/frontend/src/navbar-main.js
+++ src/main/frontend/src/navbar-main.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleRight, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import './nav.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowAltCircleRight, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+// import './nav.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [location, setLocation] = useState('');
-  const [cardHidden, setCardHidden] = useState(true);
+  // const [cardHidden, setCardHidden] = useState(true);
 
-  const toggleCard = () => {
-    setCardHidden(!cardHidden);
-  };
+  // const toggleCard = () => {
+  //   setCardHidden(!cardHidden);
+  // };
 
   const handleLocationClick = () => {
     if (location !== null && location !== '') {
@@ -20,98 +21,99 @@ const Navbar = () => {
   };
 
   return (
-      <nav className="navbar navbar-expand-lg nav-main navbar-dark" id="nav-main">
-        <div className="container-fluid">
-          <img
-              src="https://www.linkpicture.com/q/logo-exp-light.png"
-              alt="Foodex Logo"
-              width="120px"
-              style={{ marginLeft: '40px' }}
-          />
-          <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse"></div>
-          <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="aooutus">
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="contact">
-                  Contact and Support
-                </a>
-              </li>
-              <li className="nav-item">
-                {location !== null && location !== '' ? (
-                    <a className="nav-link" href="browse" onClick={handleLocationClick}>
-                      Browse Items
-                    </a>
-                ) : (
-                    <a className="nav-link" href="#" onClick={() => alert('Please set location')}>
-                      Browse Items
-                    </a>
-                )}
-              </li>
-              <li className="nav-item">
-                {location !== null && location !== '' ? (
-                    <a className="nav-link" href="restaurants" onClick={handleLocationClick}>
-                      Browse Restaurants
-                    </a>
-                ) : (
-                    <a className="nav-link" href="#" onClick={() => alert('Please set location')}>
-                      Browse Restaurants
-                    </a>
-                )}
-              </li>
-            </ul>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
-              {!1 ? (
-                  <>
-                    <li className="nav-item">
-                      <a className="nav-link nav-reg" href="login">
-                        Log In
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link nav-reg" href="signup">
-                        Sign Up
-                      </a>
-                    </li>
-                  </>
+    <nav className="navbar navbar-expand-lg nav-main navbar-dark" id="nav-main">
+      <div className="container-fluid">
+        <img
+          // src="https://www.linkpicture.com/q/logo-exp-light.png"
+          src="https://i.ibb.co/FgZkcNT/logo-exp.png"
+          alt="Foodex Logo"
+          width="120px"
+          style={{ marginLeft: '40px' }}
+        />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse"></div>
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="aboutus">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact and Support
+              </Link>
+            </li>
+            <li className="nav-item">
+              {location !== null && location !== '' ? (
+                <Link className="nav-link" to="/browseitems" onClick={handleLocationClick}>
+                  Browse Items
+                </Link>
               ) : (
-                  <>
-                    <li className="nav-item">
-                      <a className="nav-link nav-reg" href="login">
-                        Log In
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link nav-reg" href="register">
-                        Sign Up
-                      </a>
-                    </li>
-                  </>
+                <Link className="nav-link" to="/browseitems" onClick={() => alert('Please set location')}>
+                  Browse Items
+                </Link>
               )}
-            </ul>
-          </div>
+            </li>
+            <li className="nav-item">
+              {location !== null && location !== '' ? (
+                <Link className="nav-link" to="/browserestaurants" onClick={handleLocationClick}>
+                  Browse Restaurants
+                </Link>
+              ) : (
+                <Link className="nav-link" to="/browserestaurants" onClick={() => alert('Please set location')}>
+                  Browse Restaurants
+                </Link>
+              )}
+            </li>
+          </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+            {!1 ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link nav-reg" href="login" style={{ backgroundColor: 'grey', color: 'white' }}>
+                    Log In
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link nav-reg" href="signup" style={{ backgroundColor: 'grey', color: 'white' }}>
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link nav-reg" href="login" style={{ backgroundColor: 'grey', color: 'white' }}>
+                    Log In
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link nav-reg" href="register" style={{ backgroundColor: 'grey', color: 'white'}}>
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
