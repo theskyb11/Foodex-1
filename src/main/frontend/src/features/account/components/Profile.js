b17a123474d2395959f404a5d7739d4ab2ec66e5
neerajbelsare <belsareneeraj@gmail.com>
2023-07-19 23:45:34 +0530

    Modified Home, Login and Sign up

---
+++ src/main/frontend/src/features/account/components/Profile.js
@@ -0,0 +1,13 @@
+import {useEffect} from "react";
+import Navbar from "../../../../src/layouts/Navbar";
+// import WithAuth from "../components/CheckLogin";
+
+const Profile = () => {
+    return (
+        <div className={"font-sans"}>
+            <h3 className={"font-bold mt-8 ml-8"}>My Profile</h3>
+        </div>
+    )
+}
+
+export default Profile
