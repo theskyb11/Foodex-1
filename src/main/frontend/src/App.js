d2e6fd60e3b377f70f72a1cb5094a4ccc734939d
mrinal4703 <mrinal.gupta@mitaoe.ac.in>
2023-08-03 22:18:06 +0530

    Items.js added

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -7,6 +7,7 @@ import Profile from "./pages/Account";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Items from "./pages/Items";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/home" element={<Home />} />
                     <Route exact path="/contact" element={<Contact />} />
+                    <Route exact path="/items" element={<Items />} />
                     <Route exact path="/register" element={<Signup />} />
                     <Route exact path="/login" element={<Login />} />
                     <Route exact path="/account" element={<Profile />} />
